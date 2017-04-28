a9ee820364418ed3dd0a85e84b784f7f5bf3ece6
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-04-28 15:52:06 -0700

    working

--- app.js
+++ app.js
@@ -8,16 +8,34 @@ Vue.component('cheese', {
     `
 })
 
+Vue.component('button-counter', {
+  template: '<button v-on:click="increment">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment: function () { //this is just to fire the $emit
+      this.$emit('rice') //this is the event on the parent fires event on the upper level named what you give here
+    }
+  },
+})
+
 new Vue({
     el:'#noob',
     data:{
         message: 'Vue Stuff',
         swap: 'Change me',
-        drink:''
+        drink:'this is dynamic value of props',
+        total: 0
     },
     methods:{
         changeMe: function(){
             this.swap = 'Jim'
+        },
+        incrementTotal: function () {
+            this.total += 1
         }
     },
 })
